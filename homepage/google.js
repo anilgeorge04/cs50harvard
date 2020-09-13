8b12670dbd9006a731836c04c0ee74a8209c7b36
anilg <62422148+anilgeorge04@users.noreply.github.com>
2020-09-13 20:56:00 +0530

    add bootstrap library and parse HTML validation
    bootstrap: added alert with google.html file
    edits made on validation: _isblank changed to _blank, label unused value dropped

--- homepage/google.js
+++ homepage/google.js
@@ -3,10 +3,12 @@ var errormsg = document.querySelector('#errormsg');
 document.querySelector('form').onsubmit = function () {
     if (document.getElementById('query').value == "") {
         errormsg.textContent = "Can't read your mind here 😞";
+        errormsg.style.visibility = "visible";
         window.setTimeout(function () { alert('🤙 Use words master'); }, 1500);
         return false;
     }
     else {
+        errormsg.style.visibility = "hidden";
         errormsg.textContent = "";
     }
 }
